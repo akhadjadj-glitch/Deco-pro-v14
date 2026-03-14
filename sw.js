fb7a96341049402af43070b02279f9ea5bd27e3e
akhadjadj-glitch <ak.hadjadj@gmail.com>
2026-03-14 01:47:25 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_VERSION = 'decor-pro-v13';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_VERSION; })
+             .map(function(name) { return caches.delete(name); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
