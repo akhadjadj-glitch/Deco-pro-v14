55f54592b162cc564e3f8fae2ed21b7cbdce61eb
Charlotte Niel <charlotteniel@MacBookAir.lan>
2026-03-22 02:15:21 +0100

    Initial commit: Décor Pro v14 app

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE_VERSION = 'decor-pro-v14';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  // Ne jamais cacher - toujours reseau direct
+  return;
+});
